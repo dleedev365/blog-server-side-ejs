620b5210c27601136761fd335aa6c3900a5320b9
dleedev365 <dlee.dev365@gmail.com>
2024-07-25 21:20:35 -0700

    first commit

---
+++ server/routes/admin.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+const authMiddleWare = (req,res,next) => {
+    const token = req.cookies.token;
+   
+    if(!token){
+        return res.status(401).json({message: 'UnAuthorized'});
+    }
+
+    try{
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    }catch(error){
+        res.status(401).json({message: "UnAuthorized"})
+    }
+};
+
+
+router.get('/admin', async (req,res) => {
+    try{
+        const locals = {
+            title: "Admin",
+            description: "Simple Blog created with NodeJs, Express & MongoDB."
+        }
+
+        res.render('admin/index', {locals, layout: adminLayout});
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+router.post('/admin', async (req,res) => {
+    try{
+        const {username, password} = req.body;
+        const user = await User.findOne({ username});
+
+        if (!user){
+            return res.status(401).json({ message: "Invalid credentials"});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({userId: user._id}, jwtSecret);
+        res.cookie('token', token, {httpOnly: true});
+        res.redirect('/dashboard');
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+router.get('/dashboard', authMiddleWare,  async (req,res) => {
+    res.render('admin/dashboard');
+});
+
+
+router.post('/register', async (req,res) => {
+    try{
+        const {username, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try{
+            const user = await User.create({username, password: hashedPassword});
+            res.status(201).json({message: 'User Created', user});
+        } catch(error){
+            if(error.code == 11000){
+                res.status(409).json({message: 'User Already In Use'})
+            }
+            res.status(500).json({message: 'Internal Server Error'});
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+module.exports = router;
